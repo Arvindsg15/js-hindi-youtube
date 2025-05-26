db43e3559dd9441cae282fa35277cc630650f9e7
Arvind <51391415+Arvindsg15@users.noreply.github.com>
2025-05-26 13:20:16 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -30,4 +30,34 @@ console.log(booleanIsLoggedIn)
 //"" => false
 
 
+// *********************************************************** Operations ****************************************
+
+let value= 3;
+
+let negValue = -value
+console.log(negValue)
+
+let str1 ="hello"
+let str2 =" Arvind"
+
+let str3=str1 + str2
+console.log(str3) 
+
+// "1" + 2 output is 12
+// 1 + "2" output is 12
+// "1" +2 +2 output is 122
+//1 + 2 + "2" output is 32. if sting is first everything is treated as string , if string is last means first it caculates and then it treats as string like this 1 + 2 +"2" = 32
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+
 
