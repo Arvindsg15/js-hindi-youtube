9f9930b9092494ab3609a5c6b310074c6de764ce
Arvind <51391415+Arvindsg15@users.noreply.github.com>
2025-05-26 13:00:12 +0000

    dataTypes discussion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Arvind")
+let name = "Arvind"
+let age = 18
+let isLoggesIn= false
+let state = null
+
+
+// number  =>2 to pwr 53
+//bigint 
+//string => ""
+//boolean = > true/false
+//null => standalone value
+// undefined => still value is not assigned
+// symbol => unique
+
+
+//interview questions
+// type of null is object
+//type of undefined is undefined
+
+//object 
+
+console.log(typeof null)
